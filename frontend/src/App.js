82d1918836191352371266ae1bfb28a33a81232a
Hoang Nguyen <61525239+minhnguyen1408@users.noreply.github.com>
2024-10-07 13:03:25 -0400

    added display values, need style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import GetPhotos from './pages/GetPhotos';
+import GetPhotos from './pages/CTR/GetPhotos';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/get-photos" element={< GetPhotos/>} />
+        <Route path="/ctr" element={< GetPhotos/>} />
       </Routes>
     </Router>
   );
