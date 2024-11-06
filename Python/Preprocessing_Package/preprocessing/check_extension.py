ed14fcf7fd46931a5ee387fbd52c7fdbb8605bb7
jzry <jerzdb@gmail.com>
2024-11-06 16:02:42 -0500

    modified check_extension.py so that it can convert pdf to jpg and heic files to jpg.

--- Python/Preprocessing_Package/preprocessing/check_extension.py
+++ Python/Preprocessing_Package/preprocessing/check_extension.py
@@ -1,6 +1,9 @@
-# Imports
 import cv2 as cv
 import os
+import numpy as np
+from pdf2image import convert_from_path
+from PIL import Image
+from pillow_heif import register_heif_opener
 
 """
 Function Brief: Checks the file extension of an image file. If the extension is not one of the allowed types 
@@ -23,14 +26,31 @@ def checkExtension(image_path):
     if extension not in allowed_extensions:
         print(f"File extension '.{extension}' is not supported. Converting to JPG...")
 
-        image = cv.imread(image_path)
+        if extension == 'heic' or extension == 'HEIC':
 
-        # Define the new file path
-        new_file_path = os.path.splitext(image_path)[0] + '.jpg'
-        cv.imwrite(new_file_path, image)
+            # Register HEIF opener with Pillow
+            register_heif_opener()
+
+            # Open the HEIC file using Pillow
+            image = Image.open(image_path)
+
+            # Convert the image to a numpy array that OpenCV can use
+            img_np = cv.cvtColor(np.array(image), cv.COLOR_RGB2BGR)
+
+            # Define the new file path
+            new_file_path = os.path.splitext(image_path)[0] + '.jpg'
+            cv.imwrite(new_file_path, img_np)
+
+        if extension == 'pdf' or extension == 'PDF':
+
+            # Convert the PDF to images
+            images = convert_from_path(image_path)
+
+            # Save the images as JPG files
+            new_file_path = os.path.splitext(image_path)[0] + '.jpg'
+            image.save(new_file_path, 'JPEG')
 
         return new_file_path
     else:
         print(f"File extension '.{extension}' is supported.")
         return image_path
-
