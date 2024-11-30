97b4771088b5712680ebef771cebba92f3f1f372
Paul Puhnaty <pa764915@ucf.edu>
2024-11-29 22:50:39 -0500

    Changed column skip parameter of DigitGetter

--- Python/OCR_Package/OCR/okra.py
+++ Python/OCR_Package/OCR/okra.py
@@ -18,7 +18,7 @@ class DigitGetter:
         debug_images (bool): Output the input image after the preprocessing
                              stage (default=False).
         column_skip (int): The number of image columns to be skipped each loop
-                           of the scan (default=5).
+                           of the scan (default=2).
         fraction_padding (float): The minimum fractional percentage of the
                                   segmented image's height or width that
                                   should be padding (default=0.2).
@@ -37,7 +37,7 @@ class DigitGetter:
 
         # Set default attributes
         self.debug_images = False
-        self.column_skip = 3
+        self.column_skip = 2
         self.fraction_padding = 0.2
         self.find_decimal_points = True
         self.find_minus_signs = False
