fa31f5994ba7c9818f75438ab75da1b634b2160f
Paul Puhnaty <pa764915@ucf.edu>
2025-04-15 14:45:04 -0400

    Fixed Python unit tests They were trying to use LitServe and crashing, so I disabled LitServe for the unit tests.

--- backend/PythonPackages/tests/context.py
+++ backend/PythonPackages/tests/context.py
@@ -2,5 +2,11 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from io import StringIO
+from dotenv import load_dotenv
+
+config = StringIO("BYPASS_LITSERVE=1")
+load_dotenv(stream=config)
+
 import OCR
 import preprocessing
