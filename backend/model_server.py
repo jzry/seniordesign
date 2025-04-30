75357e25daf43545e7acc6abb6dae9d75fbcee19
jzry <50152663+jzry@users.noreply.github.com>
2025-04-30 14:57:02 -0400

    Updated model_server.py to bind port for render
    Kept the original line to rollback in case this breaks things.

--- backend/model_server.py
+++ backend/model_server.py
@@ -10,5 +10,7 @@ load_dotenv()
 if __name__ == '__main__':
     api = OkraLitAPI()
     server = litserve.LitServer(api)
-    server.run(port=os.environ.get('LITSERVE_PORT', 8000))
+    # server.run(port=os.environ.get('LITSERVE_PORT', 8000)) # Original
+    port = int(os.environ.get("PORT", 8000)) # default to 8000 locally
+    server.run(host="0.0.0.0", port=port)
 
